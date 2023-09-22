3555a205d5d4656bff6582c695768ba50007dd0a
kyi108 <kyi108@naver.com>
2023-09-22 10:10:56 +0900

    hello 클래스 수정하기

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -3,5 +3,6 @@ package ch01;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("===Hello 졸리다====");
+        System.out.println("===Hello02 진짜 졸리다====");
     }
 }
