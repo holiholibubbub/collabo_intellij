feff15a1fdab45fe98a5f647b6991fc09a2db01a
kyi108 <kyi108@naver.com>
2023-09-22 10:15:28 +0900

    2개의 파일이 수정되었따

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("===Hello 졸리다====");
         System.out.println("===Hello02 진짜 졸리다====");
+        System.out.println("===Hello03 진짜 진짜 졸리다====");
     }
 }
--- src/ch01/World.java
+++ src/ch01/World.java
@@ -4,5 +4,6 @@ public class World {
     public static void main(String[] args) {
         
         System.out.println("===world 졸리다 졸리ㅏㄷ====");
+        System.out.println("===world02 졸리다 졸리ㅏㄷ====");
     }
 }
